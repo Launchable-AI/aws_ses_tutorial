3c2872aefd3398cf8a9aefe328919e5edb9c1757
koreymacdougall <korey@koreymacdougall.com>
2021-06-27 02:48:45 -0400

    Initial commit

---
+++ ses_template_with_attachments/lambda_function.py
@@ -0,0 +1,35 @@
+import json
+import os
+import sys
+import traceback
+
+from pathlib import Path
+
+import boto3
+from botocore.exceptions import ClientError
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+from email_composer import Email
+
+def lambda_handler(event, context):
+
+    try:
+
+        email = Email(event)
+        email.generator_wrapper()
+        email.send_email()
+
+        return {
+            'StatusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
+
+
+    except Exception as e:
+        print("Error in main try/except of sendEmail")
+        print(e)
+        traceback.print_exc()
+        sys.exit(1)
